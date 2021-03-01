0d828afdfc56e53df71c46ba7ac2c51942861891
Damien DURO <damien@weweb.io>
2021-03-01 21:25:53 +0100

    add delete plugin in popup config

--- src/popups/configuration.popup.vue
+++ src/popups/configuration.popup.vue
@@ -11,6 +11,15 @@
             placeholder="https://my-strapi-url.com"
             v-model="settings.privateData.url"
         />
+        <div class="strapi-configuration__delete-zone danger-zone">
+            <span class="strapi-configuration__delete-zone-label">DANGER ZONE</span>
+            <button
+                class="strapi-configuration__delete-zone-button ww-editor-button -primary -red -small"
+                @click="deletePlugin"
+            >
+                Delete plugin
+            </button>
+        </div>
     </div>
 </template>
 
@@ -43,6 +52,41 @@ export default {
         },
     },
     methods: {
+        async deletePlugin() {
+            const plugin = wwLib.wwPlugins.pluginStrapi;
+            const confirm = await wwLib.wwModals.open({
+                title: {
+                    en: 'Delete plugin Strapi?',
+                    fr: 'Supprimer le plugin Strapi ?',
+                },
+                text: {
+                    en: 'Are you sure you want to delete the plugin from your website?',
+                    fr: 'Voulez-vous vraiment supprimer le plugin de votre site ?',
+                },
+                buttons: [
+                    {
+                        text: {
+                            en: 'Cancel',
+                            fr: 'Annuler',
+                        },
+                        color: '-secondary',
+                        value: false,
+                        escape: true,
+                    },
+                    {
+                        text: {
+                            en: 'Delete',
+                            fr: 'Supprimer',
+                        },
+                        color: '-primary -red',
+                        value: true,
+                        enter: true,
+                    },
+                ],
+            });
+            if (!confirm) return;
+            await wwLib.wwPlugin.deleteDesignPlugin(plugin.id);
+        },
         async beforeNext() {
             this.options.setLoadingStatus(true);
             try {
@@ -89,6 +133,18 @@ export default {
         }
         &__input {
             margin-bottom: var(--ww-spacing-03);
+        }
+        &__delete-zone {
+            margin-top: auto;
+            &-label {
+                width: 100%;
+                font-size: var(--ww-font-size-04);
+                color: var(--ww-color-red-500);
+            }
+            &-button {
+                margin-top: var(--ww-spacing-02);
+                margin-right: var(--ww-spacing-02);
+            }
         }
     }
 }
